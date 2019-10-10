2dbd722d06f8fe05a6d3f6efaf945411faf314e2
Matthew <matthew.mcpherson1@ontariotechu.net>
2019-10-10 11:57:31 -0400

    added random num gen file
    don't use its already in the main file

---
+++ Coordinates gen GDW/Source.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+// not the one we are using
+int main()
+{
+	int x, y;
+	x = rand() % 10 + 1;
+	y = rand() % 10 + 1;
+
+	cout << x << "\n" << y;
+
+	return 0;
+}
